0494050b5f49d349f70c8ee4a6ecce41a18d8e76
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2024-01-31 18:05:18 -0500

    Create Hypotenuse.c

---
+++ Hypotenuse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    double A, B, C;
+
+    printf("Enter side A: \n");
+    scanf("%lf", &A);
+
+    printf("Enter side B: \n");
+    scanf("%lf", &B);
+
+    printf("Enter side C: \n");
+    scanf("%lf", &C);
+
+    return 0;
+}
